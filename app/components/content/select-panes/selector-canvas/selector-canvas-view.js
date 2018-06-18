f52454b67fa4d7e8bda4a7769b7739d299d0d8df
Tim Lindgren <contact@timlindgren.com>
2018-06-18 11:10:35 -0600

    Renaming templates folders

---
+++ app/components/content/select-panes/selector-canvas/selector-canvas-view.js
@@ -0,0 +1,7 @@
+Mediakron.ContentBrowser.LTI = Mediakron.ContentBrowser.View.extend({
+    className: 'lti-browser',
+    template: JST['components.select-panes.selector-canvas'],
+    title: 'Add Mediakron Content',
+    context: 'lti',
+    help: ''
+});
