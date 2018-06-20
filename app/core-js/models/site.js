5628d42a64d708b8c8eefd212750dc8976358f56
Brad Mering <brad.mering@gmail.com>
2018-06-20 16:29:23 -0600

    Fixing mistake in theme commit

--- app/core-js/models/site.js
+++ app/core-js/models/site.js
@@ -1,9 +1,12 @@
 import Model from "~/core-js/extensions/models";
 class Site extends Model {
-    constructor() {
-        super({
-            
-        });
-    }
+  constructor() {
+    super({
+      id: 0,
+      name: "",
+      role: "guest",
+      urlRoot: Mediakron.Data.models.group ? Mediakron.Data.models.group : ""
+    });
+  }
 }
 export default Site;
--- app/theme/theme.js
+++ app/theme/theme.js
@@ -5,59 +5,38 @@ import ClassManagement from "class.management"
  * Helper functions for getting good colors when setting the site colros
  */
 class Theme {
-  constructor() {}
+  constructor() { }
 
   Initialize() {
-    if (Mediakron.Site.Appearance.navigation)
-      ClassManagement.swap(
-        "default-nav menu-vertical menu-horizontal",
-        Mediakron.Site.Appearance.navigation
-      );
-    $("#site-name a").text(Mediakron.Site.name);
-    $("#branding a").click(Mediakron.linkHandle);
-    $("#copyright").text(
-      Mediakron.Site.copyright
-    ); /* Sidebar menu - mobile */
-    $(".mobile-nav-button").click(function() {
-      $("#navbar").toggleClass("is-visible");
-      $(".fade-screen").toggleClass("is-visible");
-      //            $('#main-container').toggleClass('content-push-mobile');
-    }); /* Sidebar menu - large screen  */
-    $(".fade-screen, #navbar.is-visible .close-button-mobile").click(
-      function() {
-        $("#navbar").removeClass("is-visible");
-        $(".fade-screen").removeClass("is-visible");
-        //            $('#main-container').removeClass('content-push-mobile');
-      }
-    ); /* Full-screen view    */
-    $(".fullscreen-nav-toggle").click(function() {
-      $("#header").toggleClass("is-visible");
-    }); /* Sidebar menu - large screen  */
-    $(".fade-screen").click(function() {
-      $(".fade-screen").removeClass("is-visible");
-      $("#main-container").removeClass("content-push");
-    });
-    $(".toggle-search").click(function() {
-      $(".search-pane")
-        .removeClass("closed")
-        .addClass("opened"); /* close search overlay */
-      $("#search-field").focus(); /* give the search box focus  */
-    });
-    $("#search .close-button").click(function() {
-      $(".search-pane")
-        .removeClass("opened")
-        .addClass("closed"); /* close search overlay */
-      $(".toggle-search").focus(); /* return focus to search toggle button */
-    });
+
     // Install override skin
     if (Mediakron.skins[Mediakron.Site.Appearance.skin]) {
       $("#skin").attr(
         "href",
         Mediakron.Site.cssURL +
-          "skins/" +
-          Mediakron.Site.Appearance.skin
+        "skins/" +
+        Mediakron.Site.Appearance.skin
       );
     }
+
+    // Add logo from appearance settings
+    $("#site-logo").html(Mediakron.Image.logo("medium"));
+
+    // Insert site name/ 
+    $("#branding a").click(Mediakron.linkHandle);
+
+    // Insert copyright information
+    $("#copyright").text(
+      Mediakron.Site.copyright
+    );
+
+    // Menu Style - horizontal or sidebar
+    if (Mediakron.Site.Appearance.navigation)
+      ClassManagement.swap(
+        "default-nav menu-vertical menu-horizontal",
+        Mediakron.Site.Appearance.navigation
+      );
+
     // ==== Custom appearance styles ====
     var dynamicStyles = "";
     var banner = Mediakron.Site.Appearance.colors.banner;
@@ -65,164 +44,9 @@ class Theme {
     var banner_transparent = convertHex(banner, 0);
     if (Mediakron.Site.Appearance.colors.banner) {
       // Background color set in appearance settings
-      dynamicStyles =
-        ".primary-background, #mediakron #login-submit, #login-banner, #mediakron #header, #mediakron .sticky-filters #content-filters,  #messages .alert-success, #mediakron .btn-primary, #mediakron .btn-primary a, #mediakron .btn-success, #content-filter li.search-choice, #content-user .chosen-container-single .chosen-single, .settings-pane .overlay header, .overlay header.admin-style-header, .search-pane.opened .overlay header, .site-loader#progress-bar, .edit-story .wysiwyg, .edit-story .wysiwyg-headers-inner, cite.annotation.highlight, cite.annotation.highlight, cite.annotation.contains-sup sup:hover, sup.contains-cite:hover, cite.annotation.highlight, cite.annotation.contains-sup.highlight sup, sup.contains-cite.highlight, .home-menus--grid .homepage-menu-title, #navbar .dropdown-container li.dropdown-title, .home-image--half .home-template__info, .home-image--full .home-template__info,  .timeline-category .add-category, .folder-navigation .mk-icon, .folder-navigation a:hover { background-color:" +
-        Mediakron.Site.Appearance.colors.banner +
-        "; }";
-
-      // Linear gradient background for site-loader
-      //            dynamicStyles = dynamicStyles + '.site-loader .progress { background: linear-gradient(to left, ' + Mediakron.Site.Appearance.colors.banner + ', ' + Mediakron.Site.Appearance.colors.banner + ' 20%, rgba(255,255,255,0));}';
-      //
-      // Linear gradient background fade left
-      dynamicStyles =
-        dynamicStyles +
-        ".menu-horizontal #nav-main { background: linear-gradient(to left, " +
-        banner_rgb +
-        ", " +
-        banner_rgb +
-        " 90%, " +
-        banner_transparent +
-        ");}";
-      dynamicStyles =
-        dynamicStyles +
-        ".menu-horizontal #nav-main { background: -webkit-linear-gradient(right, " +
-        banner_rgb +
-        " 0%," +
-        banner_rgb +
-        " 85%," +
-        banner_transparent +
-        " 100%);}"; /* Chrome10-25,Safari5.1-6 */
-      // Linear gradient background fade right
-      dynamicStyles =
-        dynamicStyles +
-        "h1.page-header { background: linear-gradient(to right, " +
-        banner_rgb +
-        ", " +
-        banner_rgb +
-        " 90%, " +
-        banner_transparent +
-        ");}";
-      dynamicStyles =
-        dynamicStyles +
-        "h1.page-header { background: -webkit-linear-gradient(left, " +
-        banner_rgb +
-        " 0%," +
-        banner_rgb +
-        " 85%," +
-        banner_transparent +
-        " 100%);}"; /* Chrome10-25,Safari5.1-6 */
-
-      // Provide contrasting background color
-      dynamicStyles =
-        dynamicStyles +
-        ".settings-pane .overlay .page-options .mk-icon.mk-close::before, .settings-pane .overlay .page-options .mk-icon.mk-close::after, .search-pane .overlay .page-options .mk-icon.mk-close::before, .search-pane .overlay .page-options .mk-icon.mk-close::after, .overlay header.admin-style-header .page-options .mk-icon.mk-close::before, .overlay header.admin-style-header .page-options .mk-icon.mk-close::after, .add-content .edit-story .story-edit-page-options .page-options .mk-icon.mk-close::before, .add-content .edit-story .story-edit-page-options .page-options .mk-icon.mk-close::after, .editing-enabled .story-template .story-edit-page-options .page-options .mk-icon.mk-close::before, .editing-enabled .story-template .story-edit-page-options .page-options .mk-icon.mk-close::after, #navbar .close-modal .mk-icon.mk-close::after, #navbar .close-modal .mk-icon.mk-close::before, .edit-story .wlink-tool button, .folder-navigation a:hover .mk-icon { background-color:" +
-        getContrastColor(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // Provide contrasting font color
-      dynamicStyles =
-        dynamicStyles +
-        "#mediakron .settings-pane .overlay header h2, .settings-pane .overlay header h1, #mediakron .settings-pane .overlay h2 .mk-icon, .overlay .page-options a, .overlay .page-options button, .add-story-header .page-options button.close-button, .overlay .page-options .option-help a, .add-story-header .page-options a, .add-story-header .page-options button, .search-pane.opened .overlay header h2,  .overlay header.admin-style-header h2, .overlay header.admin-style-header .mk-icon, .search-pane.opened .overlay header .mk-search, .edit-story .wysiwyg-button, .editing-enabled .story-template .story-edit-page-options .page-options .option-help a .mk-icon,  .timeline-category .add-category, .folder-previous .mk-arrow-left, .folder-navigation .mk-icon, .folder-navigation a:hover .mk-icon { color:" +
-        getContrastColor(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // border color same as background
-      dynamicStyles =
-        dynamicStyles +
-        "{ border-color:" +
-        Mediakron.Site.Appearance.colors.banner +
-        "; }";
-
-      // font color same as background
-      dynamicStyles =
-        dynamicStyles +
-        ".edit-story .wlink-tool button, #mediakron #navbar #user .mk-user.level-1, .folder-navigation a:hover .mk-icon { color:" +
-        Mediakron.Site.Appearance.colors.banner +
-        "; }";
-
-      // Provide contrast color for borders
-      dynamicStyles =
-        dynamicStyles +
-        "#navbar .main-menu .add-menu-navbar.no-menus a, .site-loader#progress-bar { border-color: " +
-        getContrastColor(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // use color if it's dark enough on white background; otherwise, use black
-      dynamicStyles =
-        dynamicStyles +
-        "cite.annotation.highlight, cite.annotation.highlight, cite.annotation.contains-sup sup:hover, sup.contains-cite:hover, cite.annotation.highlight, cite.annotation.contains-sup.highlight sup, sup.contains-cite.highlight { background:" +
-        getContrastOnWhite(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      dynamicStyles =
-        dynamicStyles +
-        ".annotation-popup .mk-icon { color:" +
-        getContrastOnWhite(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      dynamicStyles =
-        dynamicStyles +
-        ".annotation-popup, .annotation-item:hover, .annotation-item.highlight  { border-color: " +
-        getContrastOnWhite(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      dynamicStyles =
-        dynamicStyles +
-        "cite.annotation.highlight, cite.annotation.highlight, cite.annotation.contains-sup sup:hover, sup.contains-cite:hover, cite.annotation.highlight, cite.annotation.contains-sup.highlight sup, sup.contains-cite.highlight {" +
-        getContrastOnWhiteColor(Mediakron.Site.Appearance.colors.banner) +
-        " }";
-
-      // Provide tinted color for borders
-      dynamicStyles =
-        dynamicStyles +
-        " { border-color: " +
-        getTint(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // Provide tinted color for backgrounds
-      dynamicStyles =
-        dynamicStyles +
-        ".site-loader .progress  { background-color: " +
-        getTint(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // Provide tinted color for color
-      dynamicStyles =
-        dynamicStyles +
-        "{ color: " +
-        getTint(Mediakron.Site.Appearance.colors.banner) +
-        "; }";
-
-      // Provide underline color based on link color
-      dynamicStyles =
-        dynamicStyles +
-        "#mediakron .contrast-tint { border-bottom: 1px solid " +
-        Mediakron.Site.Appearance.colors.bannerlink +
-        "; }";
     }
-    var bannerlink = Mediakron.Site.Appearance.colors.bannerlink;
-    banner_rgb = convertHex(banner, 100);
-    if (Mediakron.Site.Appearance.colors.bannerlink) {
-      // Banner link color from appearance settings
 
-      dynamicStyles =
-        dynamicStyles +
-        ".secondary-font, #mediakron #login-submit, #mediakron #header a, #mediakron #header .mk-icon, #mediakron #header button, .sticky-filters#content-manage h3, .add-menu-navbar a, .add-menu-navbar .mk-icon, #messages .alert-success, #mediakron .btn-primary, #mediakron .btn-success, #login-banner a, .loading-message,  #content-filter li.search-choice,  #content-user .chosen-container-single .chosen-single span, .site-loader #progress-text, .home-menus--grid .homepage-menu-title, #navbar .dropdown-container li.dropdown-title, .home-image--full h1, .home-image--half h1, .home-image--full .subtitle, .home-image--half .subtitle  { color:" +
-        Mediakron.Site.Appearance.colors.bannerlink +
-        "; }";
-
-      dynamicStyles =
-        dynamicStyles +
-        "#navbar #user .mk-user.level-1, .secondary-menu li.level-1:hover { background:" +
-        Mediakron.Site.Appearance.colors.bannerlink +
-        "; }";
-    }
-    if (dynamicStyles) {
-      // Apply custom styles to page
-      $('<style type="text/css">' + dynamicStyles + "</style>").appendTo(
-        "head"
-      );
-    }
+    // Site font
     if (Mediakron.Site.Appearance.font) {
       // Apply custom font from appearance settings
       if (
@@ -249,23 +73,24 @@ class Theme {
         if (googlefont) {
           $("head").append(
             '<link href="https://fonts.googleapis.com/css?family=' +
-              googlefont +
-              '" rel="stylesheet">'
+            googlefont +
+            '" rel="stylesheet">'
           );
           $("body").addClass("" + bodyfont + "");
         } else {
           $("body").css(
             "font-family",
             Mediakron.Site.Appearance.fonts[
-              Mediakron.Site.Appearance.font
+            Mediakron.Site.Appearance.font
             ]
           );
         }
       }
     }
-    // Add logo from appearance settings
-    $("#site-logo").html(Mediakron.Image.logo("medium"));
+
   }
+
+  // ==== Color Functions ====
 
   /**
    * Get contrasting colors
@@ -453,7 +278,7 @@ class Theme {
     else if (r < 0) r = 0;
     var b = ((num >> 8) & 0x00ff) + amt;
     if (b > 255) b = 255;
-    else if (b < 0 ) b = 0;
+    else if (b < 0) b = 0;
     var g = (num & 0x0000ff) + amt;
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
