cc5779a9ebe3249d88161013c4eb8cecf906cac9
Tim Lindgren <contact@timlindgren.com>
2018-06-21 07:33:08 -0600

    Working prototype of dynamic styles

--- app/theme/theme.js
+++ app/theme/theme.js
@@ -2,7 +2,9 @@
 import $ from "jquery"
 import Link from "../core-js/util/link"
 import Image from "../core-js/util/image";
+import * as colorUtilities from "../core-js/util/color-utilities";
 
+//colorUtilities.getTint();
 /**
  * Helper functions for getting good colors when setting the site colros
  */
@@ -35,28 +37,33 @@ export default class Theme {
     );
 
     // Menu Style - horizontal or sidebar
-    if (Mediakron.Settings.Appearance.navigation)
+    if (Mediakron.Settings.Appearance.navigation) {
       Mediakron.ClassManagement.swap(
         "default-nav menu-vertical menu-horizontal",
         Mediakron.Settings.Appearance.navigation
       );
-
+    }
     // ==== Custom appearance styles ====
+    //var primaryColor = Mediakron.Settings.Appearance.colors.banner;
+    var primaryColor = "red";
+    var primaryColorContrast = colorUtilities.getContrastColor(primaryColor);
+    var primaryColorOnWhite = colorUtilities.getContrastOnWhite(primaryColor);
+    var primaryColorTint = colorUtilities.getTint(primaryColor);
+    //var secondaryColor = Mediakron.Settings.Appearance.colors.bannerlink;
+    var secondaryColor = "green";
+    var secondaryColorContrast = colorUtilities.getContrastColor(secondaryColor);
 
-    // var primaryColor = Mediakron.Settings.Appearance.colors.banner;
-    var primaryColor = "green"
-
-    var secondaryColor = Mediakron.Settings.Appearance.colors.bannerlink;
-
-    //var banner_rgb = this.convertHex(primary-color, 100);
-    //var banner_transparent = this.convertHex(primary-color, 0);
+    //var banner_rgb = this.convertHex(primaryColor, 100);
+    //var banner_transparent = this.convertHex(primaryColor, 0);
 
     var root = document.querySelector(':root');
-    var htmlStyle = window.getComputedStyle(root);
-    htmlStyle.getPropertyValue('--mk-primary-color'); 
-    root.style.setProperty("--mk-primary-color", "red"); // set --baseColor to red
-
-
+    root.style.setProperty("--mk-primary-color", primaryColor); 
+    root.style.setProperty("--mk-primary-color-contrast", primaryColorContrast);
+    root.style.setProperty("--mk-primary-color-onwhite", primaryColorOnWhite);
+    root.style.setProperty("--mk-primary-color-tint", primaryColorTint);
+    root.style.setProperty("--mk-secondary-color", secondaryColor);
+    root.style.setProperty("--mk-secondary-color-contrast", secondaryColorContrast);
+ 
     // Site font
     if (Mediakron.Settings.Appearance.font) {
       // Apply custom font from appearance settings
@@ -98,256 +105,5 @@ export default class Theme {
         }
       }
     }
-
-  }
-
-  link(contents, url, html){
-    return Link.link(contents, url, html);
-  }
-
-  // ==== Color Functions ====
-
-  /**
-   * Get contrasting colors
-   * @param {string} hex
-   */
-  getContrastColor(hex) {
-    var threshold = 130,
-      hRed = hexToR(hex),
-      hGreen = hexToG(hex),
-      hBlue = hexToB(hex);
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    var cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return "#000000";
-    } else {
-      return "#ffffff";
-    }
-  }
-
-  /**
-   * get contrast against white background
-   * @param {string} hex
-   */
-  getContrastOnWhite(hex) {
-    var original = hex,
-      threshold = 200,
-      hRed = hexToR(hex),
-      hGreen = hexToG(hex),
-      hBlue = hexToB(hex);
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return "#000000";
-    } else {
-      return original; /* if it's dark enough on white, return original color  */
-    }
-  }
-
-  /**
-   * use color if it's dark enough on white background; otherwise, use black
-   * @param {string} hex
-   */
-  getContrastOnWhiteColor(hex) {
-    var original = hex,
-      threshold = 200,
-      hRed = hexToR(hex),
-      hGreen = hexToG(hex),
-      hBlue = hexToB(hex);
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return "background: #000000; color:#fff";
-    } else {
-      return (
-        "background:" + original + "; color: #fff"
-      ); /* if it's dark enough on white, return original color  */
-    }
-  }
-
-  /**
-   * Get contrast Tin on light
-   * @param {string} hex
-   */
-  getContrastTintLight(hex) {
-    var threshold = 130,
-      hRed = hexToR(hex),
-      hGreen = hexToG(hex),
-      hBlue = hexToB(hex);
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return "rgba(0, 0, 0, 0.5)";
-    } else {
-      return "rgba(250, 250, 250, 0.5)";
-    }
-  }
-
-  /**
-   * Get Contrast Tint
-   * @param {*} hex
-   */
-  getContrastTint(hex) {
-    var threshold = 130;
-    (hRed = hexToR(hex)), (hGreen = hexToG(hex)), (hBlue = hexToB(hex));
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return "rgba(0, 0, 0, 0.1)";
-    } else {
-      return "rgba(250, 250, 250, 0.2)";
-    }
-  } /* http://jsfiddle.net/subodhghulaxe/t568u/  */
-
-  //
-  /**
-   * Lighten a or darken a color
-   * https://css-tricks.com/snippets/javascript/lighten-darken-color/
-   * @param {string} col
-   * @param {integer} amt
-   */
-  LightenDarkenColor(col, amt) {
-    var usePound = false;
-    if (col[0] == "#") {
-      col = col.slice(1);
-      usePound = true;
-    }
-    var num = parseInt(col, 16);
-    var r = (num >> 16) + amt;
-    if (r > 255) r = 255;
-    else if (r < 0) r = 0;
-    var b = ((num >> 8) & 0x00ff) + amt;
-    if (b > 255) b = 255;
-    else if (b < 0) b = 0;
-    var g = (num & 0x0000ff) + amt;
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-  }
-
-  /**
-   * Geta tint color
-   * @param {string} hex
-   */
-  getTint(hex) {
-    var threshold = 130,
-      hRed = hexToR(hex),
-      hGreen = hexToG(hex),
-      hBlue = hexToB(hex);
-
-    var Lighter = LightenDarkenColor(hex, 80);
-    var Darker = LightenDarkenColor(hex, -40);
-
-    function hexToR(h) {
-      return parseInt(cutHex(h).substring(0, 2), 16);
-    }
-
-    function hexToG(h) {
-      return parseInt(cutHex(h).substring(2, 4), 16);
-    }
-
-    function hexToB(h) {
-      return parseInt(cutHex(h).substring(4, 6), 16);
-    }
-
-    function cutHex(h) {
-      return h.charAt(0) == "#" ? h.substring(1, 7) : h;
-    }
-    cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
-    if (cBrightness > threshold) {
-      return Darker;
-    } else {
-      return Lighter;
-    }
-  }
-
-  /**
-   * Convert a hex color to a rgba
-   * @param {string} hex
-   * @param {float} opacity
-   */
-  convertHex(hex, opacity) {
-    hex = hex.replace("#", "");
-    var r = parseInt(hex.substring(0, 2), 16);
-    var g = parseInt(hex.substring(2, 4), 16);
-    var b = parseInt(hex.substring(4, 6), 16);
-    var result = "rgba(" + r + "," + g + "," + b + "," + opacity / 100 + ")";
-    return result;
   }
 }
-
