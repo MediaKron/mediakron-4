66e4149756a513894146b88c542200e3f94ef4dc
Tim Lindgren <contact@timlindgren.com>
2018-07-17 14:42:52 -0400

    Converting Secondary Menu view

--- app/navigation/secondary-menu/secondary-menu.js
+++ app/navigation/secondary-menu/secondary-menu.js
@@ -1,31 +1,61 @@
+import MediakronView from '~/core-js/extensions/views';
+import $ from "jquery";
+import _ from "lodash";
+import tpl  from "./secondary-menu.html";
 
-Mediakron.MenuRight = Backbone.View.extend({
-    template: JST['navigation.main.right'],
-    el: '#nav-secondary',
-    items: [],
-    tags: [],
-    fullscreen: false,
-    initialize: function () {
-        this.tags = Mediakron.items.where({ type: 'tag' });
-        var view = this;
-        Mediakron.App.Events.on('update_content', function () {
-            view.render();
-        });
-        Mediakron.App.Events.on('load:complete', function () {
-            view.render();
-        });
-        Mediakron.router.on("route", function (route, params) {
-            view.closeAllModels();
-        });
-        Mediakron.App.Events.on("context:goto", function (eventName) {
-            $(".search-pane").removeClass("opened"); /* close search overlay */
-            $('.toggle-search').focus(); /* return focus to search toggle button */
-        });
-        $(document).on('fullscreenchange', function () {
-            $('body').toggleClass('fullscreen');
-        });
-    },
-    render: function () {
+var view = false;
+
+export default class SecondaryMenu extends MediakronView {
+
+    /**
+     * The constructor for the backbone class
+     * @param {object} options 
+     */
+    constructor(options) {
+        // execute the parent options first
+        super({
+            items: [],
+            tags: [],
+            fullscreen: false,
+        })
+        this.data = {};
+            this.tags = Mediakron.items.where({ type: 'tag' });
+            var view = this;
+            Mediakron.App.Events.on('update_content', function () {
+                view.render();
+            });
+            Mediakron.App.Events.on('load:complete', function () {
+                view.render();
+            });
+            Mediakron.router.on("route", function (route, params) {
+                view.closeAllModels();
+            });
+            Mediakron.App.Events.on("context:goto", function (eventName) {
+                $(".search-pane").removeClass("opened"); /* close search overlay */
+                $('.toggle-search').focus(); /* return focus to search toggle button */
+            });
+            $(document).on('fullscreenchange', function () {
+                $('body').toggleClass('fullscreen');
+            });
+    }
+
+    // Cast the html template 
+    get template() { 
+        return _.template(tpl); 
+    }
+
+    /**
+     * 
+     * @param {object} data 
+     */
+    initialize(data) {
+        return this;
+    }
+
+    /**
+     * Render the view
+     */
+    render() {
         this.$el.hide();
         this.tags = Mediakron.items.where({ type: 'tag' });
         this.$el.html(this.template({
@@ -37,26 +67,46 @@ Mediakron.MenuRight = Backbone.View.extend({
 
         var titles = Mediakron.items.map(function (item) { return item.get('title'); });
         return this;
-    },
-    afterRender: function () { // make sure you return this at the end of an afterRender function
-    },
-    closeAllModels: function () {
+    }
+
+    get events() {
+        return {
+            'click #configure-site-menu': 'toggleConfig',
+            'click #dev-site-menu': 'toggleDev',
+            'keyup #search-field': 'doSearch',
+            'submit #search form': 'doSearch',
+            'click #full-screen': 'fullScreen',
+            'click .open-modal': 'openModal',
+            'click .close-modal': 'closeModal',
+            'click .menu-bg': 'closeModalBg',
+            'click .toggle-search': 'revealSearch',
+            'click a': Mediakron.linkHandle
+        }
+    }
+
+    /**
+     * Description
+     * @param {object} something 
+     */
+    newName(something) {
+       
+    }
+
+    /**
+     * Close All Models
+     * @param {object}
+     */
+    closeAllModels() {
         $('.secondary-menu .dropdown-container').attr({ 'data-visually-hidden': 'true' });
         $('.menu-bg').removeClass('open');
-    },
-    events: {
-        'click #configure-site-menu': 'toggleConfig',
-        'click #dev-site-menu': 'toggleDev',
-        'keyup #search-field': 'doSearch',
-        'submit #search form': 'doSearch',
-        'click #full-screen': 'fullScreen',
-        'click .open-modal': 'openModal',
-        'click .close-modal': 'closeModal',
-        'click .menu-bg': 'closeModalBg',
-        'click .toggle-search': 'revealSearch',
-        'click a': Mediakron.linkHandle
-    },
-    fullScreen: function (e) {
+    }
+
+
+    /**
+     * Fullscreen
+     * @param {object} e 
+     */
+    fullScreen(e) {
         var element = document.getElementById("mediakron"),
             $el = $(e.currentTarget);
         if (this.fullscreen) {
@@ -68,11 +118,21 @@ Mediakron.MenuRight = Backbone.View.extend({
             $el.html('<span class="mk-icon mk-fullscreen-close"></span>');
             this.fullscreen = true;
         }
-    },
-    toggleConfig: function () {
+    }
+
+    /**
+     * Toggle Config
+     * @param {object} 
+     */
+    toggleConfig() {
         $('#configure-menu').slideToggle('fast');
-    },
-    doSearch: function (e) {
+    }
+    
+    /**
+     * Search
+     * @param {object} e 
+     */
+    doSearch(some) {
         e.preventDefault();
         var searchFor = $(e.currentTarget).val();
         if (e.keyCode == '13') {
@@ -81,7 +141,16 @@ Mediakron.MenuRight = Backbone.View.extend({
             }
         }
         return false;
-    },
+    }
+
+    /**
+     * Description
+     * @param {object} something 
+     */
+    newName(something) {
+       
+    }
+
     openModal: function (e) {
         e.preventDefault();
         this.closeAllModels();
@@ -89,23 +158,156 @@ Mediakron.MenuRight = Backbone.View.extend({
         $('.menu-bg').addClass('open');
         return false;
     },
-    closeModal: function (e) {
+
+    /**
+     * Close Modal
+     * @param {object} e 
+     */
+    closeModal(e) {
         e.preventDefault();
         $(e.currentTarget).parent().attr({ 'data-visually-hidden': 'true' });
         $('.menu-bg').removeClass('open');
         return false;
-    },
-    closeModalBg: function (e) {
+    }
+
+    /**
+     * Close Modal Background
+     * @param {object} e 
+     */
+    closeModalBg(e) {
         e.preventDefault();
         $('#navbar .dropdown-container').attr({ 'data-visually-hidden': 'true' });
         $('.menu-bg').removeClass('open');
         return false;
-    },
-    toggleDev: function () {
+    }
+    
+    /**
+     * Toggle Dev Menu
+     * @param {object}
+     */
+    toggleDev() {
         $('#dev-menu').slideToggle('fast');
-    },
-    revealSearch: function () {
+    }
+
+    /**
+     * Open Search
+     * @param {object}  
+     */
+    revealSearch() {
         $(".search-pane").toggleClass("opened");
         $("#search-field").focus(); /* give the search box focus  */
     }
-});
+}
+
+// @REVIEW then, delete. Original view below
+
+// Mediakron.MenuRight = Backbone.View.extend({
+//     template: JST['navigation.main.right'],
+//     el: '#nav-secondary',
+//     items: [],
+//     tags: [],
+//     fullscreen: false,
+//     initialize: function () {
+//         this.tags = Mediakron.items.where({ type: 'tag' });
+//         var view = this;
+//         Mediakron.App.Events.on('update_content', function () {
+//             view.render();
+//         });
+//         Mediakron.App.Events.on('load:complete', function () {
+//             view.render();
+//         });
+//         Mediakron.router.on("route", function (route, params) {
+//             view.closeAllModels();
+//         });
+//         Mediakron.App.Events.on("context:goto", function (eventName) {
+//             $(".search-pane").removeClass("opened"); /* close search overlay */
+//             $('.toggle-search').focus(); /* return focus to search toggle button */
+//         });
+//         $(document).on('fullscreenchange', function () {
+//             $('body').toggleClass('fullscreen');
+//         });
+//     },
+//     render: function () {
+//         this.$el.hide();
+//         this.tags = Mediakron.items.where({ type: 'tag' });
+//         this.$el.html(this.template({
+//             secondary: Mediakron.Settings.Navigation.secondary,
+//             tags: this.tags
+//         }));
+
+//         this.$el.show();
+
+//         var titles = Mediakron.items.map(function (item) { return item.get('title'); });
+//         return this;
+//     },
+//     afterRender: function () { // make sure you return this at the end of an afterRender function
+//     },
+//     closeAllModels: function () {
+//         $('.secondary-menu .dropdown-container').attr({ 'data-visually-hidden': 'true' });
+//         $('.menu-bg').removeClass('open');
+//     },
+//     events: {
+//         'click #configure-site-menu': 'toggleConfig',
+//         'click #dev-site-menu': 'toggleDev',
+//         'keyup #search-field': 'doSearch',
+//         'submit #search form': 'doSearch',
+//         'click #full-screen': 'fullScreen',
+//         'click .open-modal': 'openModal',
+//         'click .close-modal': 'closeModal',
+//         'click .menu-bg': 'closeModalBg',
+//         'click .toggle-search': 'revealSearch',
+//         'click a': Mediakron.linkHandle
+//     },
+//     fullScreen: function (e) {
+//         var element = document.getElementById("mediakron"),
+//             $el = $(e.currentTarget);
+//         if (this.fullscreen) {
+//             Mediakron.exitFullScreen();
+//             $el.html('<span class="mk-icon mk-fullscreen-open"></span>');
+//             this.fullscreen = false;
+//         } else {
+//             Mediakron.goFullScreen(element);
+//             $el.html('<span class="mk-icon mk-fullscreen-close"></span>');
+//             this.fullscreen = true;
+//         }
+//     },
+//     toggleConfig: function () {
+//         $('#configure-menu').slideToggle('fast');
+//     },
+//     doSearch: function (e) {
+//         e.preventDefault();
+//         var searchFor = $(e.currentTarget).val();
+//         if (e.keyCode == '13') {
+//             if (searchFor.length > 1) {
+//                 Mediakron.router.navigate('search/' + searchFor, { trigger: true });
+//             }
+//         }
+//         return false;
+//     },
+//     openModal: function (e) {
+//         e.preventDefault();
+//         this.closeAllModels();
+//         $(e.currentTarget).next().attr({ 'data-visually-hidden': 'false' });
+//         $('.menu-bg').addClass('open');
+//         return false;
+//     },
+//     closeModal: function (e) {
+//         e.preventDefault();
+//         $(e.currentTarget).parent().attr({ 'data-visually-hidden': 'true' });
+//         $('.menu-bg').removeClass('open');
+//         return false;
+//     },
+//     closeModalBg: function (e) {
+//         e.preventDefault();
+//         $('#navbar .dropdown-container').attr({ 'data-visually-hidden': 'true' });
+//         $('.menu-bg').removeClass('open');
+//         return false;
+//     },
+//     toggleDev: function () {
+//         $('#dev-menu').slideToggle('fast');
+//     },
+//     revealSearch: function () {
+//         $(".search-pane").toggleClass("opened");
+//         $("#search-field").focus(); /* give the search box focus  */
+//     }
+// });
