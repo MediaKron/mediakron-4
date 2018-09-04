e021db25c8f31214768497f02d9efc48a7ebad11
Tim Lindgren <contact@timlindgren.com>
2018-09-04 18:53:52 -0400

    Converting help, export, settings, privacy, site info

--- app/help/help-view.js
+++ app/help/help-view.js
@@ -1,7 +1,50 @@
-Mediakron.HelpPage = Mediakron.Extensions.View.extend({
-    template: JST['help.default'],
-    type: false,
-    initialize: function (type) {
+import MediakronView from '~/core-js/extensions/views';
+import $ from "jquery";
+import _ from "lodash";
+import tpl  from "./templatename.html";
+
+var view = false;
+
+export default class HelpPage extends MediakronView {
+    /**
+     * The constructor for the backbone class
+     * @param {object} options 
+     */
+    constructor(options) {
+        // execute the parent options first
+        super({
+            type: false,
+        })
+        this.data = Mediakron.Settings.HomePage;
+        var view = this;
+
+        // @brad - not sure how to handle multiple templates
+        if (this.data.layout) {
+            if (JST['pages.welcome.' + this.data.layout]) {
+                this.template = JST['pages.welcome.' + this.data.layout];
+            }
+        }
+        this.item = false;
+        if (request.item) {
+            this.item = request.item;
+        }
+        this.data = {};
+            if (!this.changes) {
+                this.changes = Mediakron.Settings;
+            }
+        view = this;
+    }
+
+    // Cast the html template 
+    get template() { 
+        return _.template(tpl); 
+    }
+
+    /**
+     * 
+     * @param {object} data 
+     */
+    initialize(data) {
         this.type = type;
         this.elementParent = $('#help-context');
         if(type){
@@ -10,29 +53,68 @@ Mediakron.HelpPage = Mediakron.Extensions.View.extend({
                 this.template = JST[template];
             }
         }
-    },
-    render: function () {
+    }
+
+    /**
+     * Render the view
+     */
+    render() {
         var content = {};
         $(this.el).html(this.template(content));
         return this;
-    },
-    afterRender: function(){
-    },
-    events: {
-        'click a':                         Mediakron.linkHandle,
-        'click #close-help':               'closeHelp',
-        'click .intro-tour':        'guidedTour'
-    },
-    guidedTour:function(){
-        require(["tours/intro"],function(tour){
-            tour.start(); 
-        });
+    }
 
-    },
-    closeHelp:function(){
+    get events() {
+        return {
+            'click a':                  Mediakron.linkHandle,
+            'click #close-help':        'closeHelp',
+            'click .intro-tour':        'guidedTour'
+        }
+    }
+
+    /**
+     * Close Help Window
+     * @param {object} 
+     */
+    closeHelp() {
         this.elementParent.addClass('closed').removeClass('opened');
         Mediakron.router.navigate(this.lastRoute);
         this.currentRoute = this.lastRoute;
     }
+}
+
+// @REVIEW then, delete. Original view below
+
+// Mediakron.HelpPage = Mediakron.Extensions.View.extend({
+//     template: JST['help.default'],
+//     type: false,
+//     initialize: function (type) {
+//         this.type = type;
+//         this.elementParent = $('#help-context');
+//         if(type){
+//             var template = 'help.'+type;
+//             if(JST[template]){
+//                 this.template = JST[template];
+//             }
+//         }
+//     },
+//     render: function () {
+//         var content = {};
+//         $(this.el).html(this.template(content));
+//         return this;
+//     },
+//     afterRender: function(){
+//     },
+//     events: {
+//         'click a':                         Mediakron.linkHandle,
+//         'click #close-help':               'closeHelp',
+//         'click .intro-tour':        'guidedTour'
+//     },
     
-});
+//     closeHelp:function(){
+//         this.elementParent.addClass('closed').removeClass('opened');
+//         Mediakron.router.navigate(this.lastRoute);
+//         this.currentRoute = this.lastRoute;
+//     }
+    
+// });
--- app/site-settings/privacy/privacy-view.js
+++ app/site-settings/privacy/privacy-view.js
@@ -1,27 +1,65 @@
-Mediakron.Admin.PrivacyPage = Mediakron.Extensions.View.extend({
-    template: JST['settings.privacy'],
-    initialize: function(type) {
+import MediakronView from '~/core-js/extensions/views';
+import $ from "jquery";
+import _ from "lodash";
+import tpl  from "./privacy.html";
+
+var view = false;
+
+export default class PrivacySettings extends MediakronView {
+
+    /**
+     * The constructor for the backbone class
+     * @param {object} options 
+     */
+    constructor(options) {
+        // execute the parent options first
+        super({
+
+        })
+    }
+
+    // Cast the html template 
+    get template() { 
+        return _.template(tpl); 
+    }
+
+    /**
+     * 
+     * @param {object} data 
+     */
+    initialize(data) {
         if (!this.changes) {
             this.changes = Mediakron.Settings;
         }
-    },
-    render: function() {
+    }
+
+    /**
+     * Render the view
+     */
+    render() {
         var content = {};
         $(this.el).html(this.template(content));
         return this;
-    },
-    afterRender: function() {},
-    events: {
-        'click a': Mediakron.linkHandle,
-        'click #done-editing': Mediakron.Edit.saveSettingsForm,
-        'click .close-button': Mediakron.Edit.cancelEdit,
-        'click #close-settings-context': Mediakron.Edit.cancelEdit,
-        'blur .settings-field': "bindSettings",
-        'change .settings-field': "bindSettings",
-        'change .toggle-field': "bindToggleSettings"
-    },
-    bindSettings: function(e) {
-        var val = $(e.currentTarget).val(),
+    }
+
+    get events() {
+        return {
+            'click a': Mediakron.linkHandle,
+            'click #done-editing': Mediakron.Edit.saveSettingsForm,
+            'click .close-button': Mediakron.Edit.cancelEdit,
+            'click #close-settings-context': Mediakron.Edit.cancelEdit,
+            'blur .settings-field': "bindSettings",
+            'change .settings-field': "bindSettings",
+            'change .toggle-field': "bindToggleSettings"
+        }
+    }
+
+    /**
+     * Bind Settings
+     * @param {object} e
+     */
+    bindSettings(e) {
+       var val = $(e.currentTarget).val(),
             property = $(e.currentTarget).attr('settings-attr'),
             checked = $(e.currentTarget).is(':checked');
         if (val == 'true') {
@@ -32,11 +70,62 @@ Mediakron.Admin.PrivacyPage = Mediakron.Extensions.View.extend({
         }
         this.changes[property] = val;
         Mediakron.Status.formChanged = true;
-    },
-    bindToggleSettings: function(e) {
+    }
+
+    /**
+     * Bind Toggle Settings
+     * @param {object} e 
+     */
+    bindToggleSettings(se) {
         var property = $(e.currentTarget).attr('settings-attr'),
             checked = $(e.currentTarget).is(':checked');
         this.changes[property] = checked;
         Mediakron.Status.formChanged = true;
     }
-});
+
+}
+
+// @REVIEW then, delete. Original view below
+
+// Mediakron.Admin.PrivacyPage = Mediakron.Extensions.View.extend({
+//     template: JST['settings.privacy'],
+//     initialize: function(type) {
+//         if (!this.changes) {
+//             this.changes = Mediakron.Settings;
+//         }
+//     },
+//     render: function() {
+//         var content = {};
+//         $(this.el).html(this.template(content));
+//         return this;
+//     },
+//     afterRender: function() {},
+//     events: {
+//         'click a': Mediakron.linkHandle,
+//         'click #done-editing': Mediakron.Edit.saveSettingsForm,
+//         'click .close-button': Mediakron.Edit.cancelEdit,
+//         'click #close-settings-context': Mediakron.Edit.cancelEdit,
+//         'blur .settings-field': "bindSettings",
+//         'change .settings-field': "bindSettings",
+//         'change .toggle-field': "bindToggleSettings"
+//     },
+//     bindSettings: function(e) {
+//         var val = $(e.currentTarget).val(),
+//             property = $(e.currentTarget).attr('settings-attr'),
+//             checked = $(e.currentTarget).is(':checked');
+//         if (val == 'true') {
+//             val = true;
+//         }
+//         if (val == 'false') {
+//             val = false;
+//         }
+//         this.changes[property] = val;
+//         Mediakron.Status.formChanged = true;
+//     },
+//     bindToggleSettings: function(e) {
+//         var property = $(e.currentTarget).attr('settings-attr'),
+//             checked = $(e.currentTarget).is(':checked');
+//         this.changes[property] = checked;
+//         Mediakron.Status.formChanged = true;
+//     }
+// });
