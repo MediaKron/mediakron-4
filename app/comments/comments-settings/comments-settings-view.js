c4894de733d1db37610db45ff2295850500d646a
Tim Lindgren <contact@timlindgren.com>
2018-11-02 16:33:59 -0400

    Converting Comments settings template

--- app/comments/comments-settings/comments-settings-view.js
+++ app/comments/comments-settings/comments-settings-view.js
@@ -1,23 +1,70 @@
-Mediakron.Admin.CommentsPage = Mediakron.Extensions.View.extend({
-    template: JST['settings.comments'],
-    initialize: function() {},
-    changes: {},
-    render: function() {
+import MediakronView from '~/core-js/extensions/views';
+import $ from "jquery";
+import _ from "lodash";
+import tpl  from "./commentsettings/comments.html";
+
+var view = false;
+
+export default class CommentSettings extends MediakronView {
+
+    /**
+     * The constructor for the backbone class
+     * @param {object} options 
+     */
+    constructor(options) {
+        // execute the parent options first
+        super({
+        })
+        this.data = {};
+            if (!this.changes) {
+                this.changes = Mediakron.Settings;
+            }
+        view = this;
+    }
+
+    // Cast the html template 
+    get template() { 
+        return _.template(tpl); 
+    }
+
+    /**
+     * 
+     * @param {object} data 
+     */
+    initialize(data) {
+        return this;
+    }
+
+    /**
+     * Render the view
+     */
+    render() {
         this.changes = Mediakron.Settings;
         $(this.el).html(this.template());
         return this;
-    },
-    afterRender: function() {},
-    events: {
-        'click a': Mediakron.linkHandle,
-        'click #done-editing': Mediakron.Edit.saveSettingsForm,
-        'click .close-button': Mediakron.Edit.cancelEdit,
-        'click #close-settings-context': Mediakron.Edit.cancelEdit,
-        'blur .settings-field': "bindSettings",
-        'change .settings-field': "bindSettings",
-        'change .toggle-field': "bindToggleSettings"
-    },
-    bindSettings: function (e) {
+    }
+
+    get events() {
+        return {
+            'click a': Mediakron.linkHandle,
+            'click #done-editing': Mediakron.Edit.saveSettingsForm,
+            'click .close-button': Mediakron.Edit.cancelEdit,
+            'click #close-settings-context': Mediakron.Edit.cancelEdit,
+            'blur .settings-field': "bindSettings",
+            'change .settings-field': "bindSettings",
+            'change .toggle-field': "bindToggleSettings"
+        }
+    }
+
+    afterrender() {
+        
+    }
+
+    /**
+     * Bind Settings
+     * @param {object} e 
+     */
+    bindSettings (e) {
 
         var val = $(e.currentTarget).val(),
             property = $(e.currentTarget).attr('settings-attr'),
@@ -30,11 +77,60 @@ Mediakron.Admin.CommentsPage = Mediakron.Extensions.View.extend({
         }
         this.changes[property] = val;
         Mediakron.Status.formChanged = true;
-    },
-    bindToggleSettings: function (e) {
+    }
+
+    /**
+     * Bind Toggle Settings
+     * @param {object} e 
+     */
+    bindToggleSettings(e) {
         var property = $(e.currentTarget).attr('settings-attr'),
             checked = $(e.currentTarget).is(':checked');
         this.changes[property] = checked;
         Mediakron.Status.formChanged = true;
     }
-});
+
+}
+
+// @REVIEW then, delete. Original view below
+
+// Mediakron.Admin.CommentsPage = Mediakron.Extensions.View.extend({
+//     template: JST['settings.comments'],
+//     initialize: function() {},
+//     changes: {},
+//     render: function() {
+//         this.changes = Mediakron.Settings;
+//         $(this.el).html(this.template());
+//         return this;
+//     },
+//     afterRender: function() {},
+//     events: {
+//         'click a': Mediakron.linkHandle,
+//         'click #done-editing': Mediakron.Edit.saveSettingsForm,
+//         'click .close-button': Mediakron.Edit.cancelEdit,
+//         'click #close-settings-context': Mediakron.Edit.cancelEdit,
+//         'blur .settings-field': "bindSettings",
+//         'change .settings-field': "bindSettings",
+//         'change .toggle-field': "bindToggleSettings"
+//     },
+//     bindSettings: function (e) {
+
+//         var val = $(e.currentTarget).val(),
+//             property = $(e.currentTarget).attr('settings-attr'),
+//             checked = $(e.currentTarget).is(':checked');
+//         if (val == 'true') {
+//             val = true;
+//         }
+//         if (val == 'false') {
+//             val = false;
+//         }
+//         this.changes[property] = val;
+//         Mediakron.Status.formChanged = true;
+//     },
+//     bindToggleSettings: function (e) {
+//         var property = $(e.currentTarget).attr('settings-attr'),
+//             checked = $(e.currentTarget).is(':checked');
+//         this.changes[property] = checked;
+//         Mediakron.Status.formChanged = true;
+//     }
+// });
