50e7475837fe83af3b2d6c70ddabcdeaffc7bac9
Austin Bailey <baileyau@bc.edu>
2018-11-14 12:41:58 -0500

    main-menu-view

--- app/navigation/main-menu/main-menu-view.js
+++ app/navigation/main-menu/main-menu-view.js
@@ -1,71 +1,48 @@
-
+import MediakronView from '~/core-js/extensions/views';
+import $ from "jquery";
 import _ from "lodash";
-var template = require("./main-menu.html")
+import tpl  from "./main-menu.html";
 
+var view = false;
 
-export default class MainMenu extends Backbone.View {
+export default class MainMenu extends MediakronView {
+
     /**
      * The constructor for the backbone class
-     * @param {object} options 
+     * @param {object} options
      */
-    constructor(options){
+    constructor(options) {
         // execute the parent options first
-        super(options);
-        //this.template = template;
-        this.el = '#nav-main';
-        this.items = [];
-        this.tags = [];
-        this.items = [];
+        super({
+            className: 'MainMenu',
+            data: false,
+            item: false,
+        })
+        this.data = {};
+            if (!this.changes) {
+                this.changes = Mediakron.Settings;
+            }
+        view = this;
     }
 
-    // Cast the html template 
-    get template() { return _.template(template); }
-    
+    // Cast the html template
+    get template() {
+        return _.template(tpl);
+    }
+
     /**
-     * This should initialize the view
+     *
+     * @param {object} data
      */
-    initialize(){
-        this.topics = Mediakron.topics;
-        /*Mediakron.App.Events.on("context:goto", function (event) {
-            var title = '';
-            if (event.view.model) {
-                title = event.view.model.get('title') + ' | ';
-
-            }
-            title = title + Mediakron.Settings.name;
-            $('title').text(title);
-        });*/
-        var view = this;
-        //Mediakron.App.Events.on('update_content', function () {
-        view.render();
-        //});
+    initialize(data) {
+        return this;
     }
 
-    render(){
-        var items = Mediakron.Settings.Navigation.primary,
-            length = items.length,
-            i = 0,
-            item;
-        this.items = [];
-        for (i; i < length; i++) {
-            if (items[i] === null) { continue; }
-            if (typeof (items[i]) == 'object') {
-                if (items[i].type == 'external') {
-                    this.items.push(items[i]);
-                }
-            } else if (typeof (items[i]) == 'string') {
-                this.items.push(Mediakron.getItemFromURI(items[i]));
-            }
-        }
-        this.$el.hide();
-        this.$el.html(this.template({
-            topics: this.topics,
-            'items': this.items,
-            'tags': this.tags,
-            secondary: Mediakron.Settings.Navigation.secondary
-        }));
-        this.$el.show();
-
+    /**
+     * Render the view
+     */
+    render() {
+        this.$el.html(this.template(this.data)).addClass(this.className);
         return this;
     }
 
@@ -79,6 +56,10 @@ export default class MainMenu extends Backbone.View {
         }
     }
 
+    afterrender() {
+
+    }
+
     toggleMenu(){
         $('.js-menu,.js-menu-screen', this.parent).toggleClass('is-visible');
         //         $('#main-container').toggleClass('content-push');
@@ -90,3 +71,97 @@ export default class MainMenu extends Backbone.View {
     }
 
 }
+
+// @REVIEW then, delete. Original view below
+
+// import _ from "lodash";
+// var template = require("./main-menu.html")
+//
+//
+// export default class MainMenu extends Backbone.View {
+//     /**
+//      * The constructor for the backbone class
+//      * @param {object} options
+//      */
+//     constructor(options){
+//         // execute the parent options first
+//         super(options);
+//         //this.template = template;
+//         this.el = '#nav-main';
+//         this.items = [];
+//         this.tags = [];
+//         this.items = [];
+//     }
+//
+//     // Cast the html template
+//     get template() { return _.template(template); }
+//
+//     /**
+//      * This should initialize the view
+//      */
+//     initialize(){
+//         this.topics = Mediakron.topics;
+//         /*Mediakron.App.Events.on("context:goto", function (event) {
+//             var title = '';
+//             if (event.view.model) {
+//                 title = event.view.model.get('title') + ' | ';
+//
+//             }
+//             title = title + Mediakron.Settings.name;
+//             $('title').text(title);
+//         });*/
+//         var view = this;
+//         //Mediakron.App.Events.on('update_content', function () {
+//         view.render();
+//         //});
+//     }
+//
+//     render(){
+//         var items = Mediakron.Settings.Navigation.primary,
+//             length = items.length,
+//             i = 0,
+//             item;
+//         this.items = [];
+//         for (i; i < length; i++) {
+//             if (items[i] === null) { continue; }
+//             if (typeof (items[i]) == 'object') {
+//                 if (items[i].type == 'external') {
+//                     this.items.push(items[i]);
+//                 }
+//             } else if (typeof (items[i]) == 'string') {
+//                 this.items.push(Mediakron.getItemFromURI(items[i]));
+//             }
+//         }
+//         this.$el.hide();
+//         this.$el.html(this.template({
+//             topics: this.topics,
+//             'items': this.items,
+//             'tags': this.tags,
+//             secondary: Mediakron.Settings.Navigation.secondary
+//         }));
+//         this.$el.show();
+//
+//         return this;
+//     }
+//
+//     get events() {
+//         return {
+//             'click .js-menu-trigger': 'toggleMenu',
+//             'click .js-menu-screen': 'toggleMenu',
+//             'click .main-menu-sidebar .close-button': 'closeMenu',
+//             'click a': Mediakron.linkHandle,
+//             'click .main-menu-sidebar.is-visible a': 'toggleMenu'
+//         }
+//     }
+//
+//     toggleMenu(){
+//         $('.js-menu,.js-menu-screen', this.parent).toggleClass('is-visible');
+//         //         $('#main-container').toggleClass('content-push');
+//     }
+//
+//     closeMenu(){
+//         $('.js-menu,.js-menu-screen', this.parent).removeClass('is-visible');
+//         //         $('#main-container').toggleClass('content-push');
+//     }
+//
+// }
