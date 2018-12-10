7080d6c5609571a742b265a56d653043dbaf3c6c
Austin Bailey <baileyau@bc.edu>
2018-12-10 11:42:07 -0500

    Added demo examples to Profile.vue

--- app/views/Profile.vue
+++ app/views/Profile.vue
@@ -5,8 +5,17 @@
         <p>Email: {{ user.email }}</p>
         <p>Sites</p>
         <div v-for="(site, index) in sites" :key="index">
-            <p>{{ site.name }} {{ site.role }}</p>
+            <p>{{ site.name }} | {{ site.role }}</p>
         </div>
+        <div v-if="render">
+            hello
+        </div>
+        <button @click="render = !render">Toggle v-if</button>
+        <form @submit.prevent="submitForm">
+            <input type="text" v-model="form.name">
+            <input type="text" v-model="form.role">
+            <button type="submit" name="button">Submit fake form</button>
+        </form>
     </div>
 </template>
 
@@ -29,18 +38,38 @@ export default {
                     name: 'Mediakron Demo',
                     role: 'viewer'
                 }
-            ]
+            ],
+            render: true,
+            form: {
+                name: '',
+                role: '',
+            }
         }
     },
     created() {
         // Runs immediately, before HTML is rendered
         console.log(this.user.email)
+        this.customFunction()
     },
     mounted() {
         // Runs after the DOM is mounted, after created()
+    },
+    methods: {
+        // Methods for whatever
+        customFunction() {
+            // Do whatever
+            console.log("In custom function")
+        },
+        submitForm(event) {
+            console.log(this.form)
+        }
+    },
+    computed: {
+        // Basically replaces mustache code to clean it up
     }
 }
 </script>
 
 <style lang="css">
+
 </style>
