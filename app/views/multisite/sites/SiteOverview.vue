3293bb15b8d7fe794f5cb9f679c7f688a461bbc2
Austin Bailey <baileyau@bc.edu>
2018-12-18 16:37:20 -0500

    Created admin site overview page

---
+++ app/views/multisite/sites/SiteOverview.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+    <div class="container mt-5">
+        <b-card-group deck class="mb-3">
+            <b-card :title="'Site Name: ' + currentSite.title"
+                    header-tag="header"
+                    footer-tag="footer">
+                <h6 slot="header" class="mb-0">Details</h6>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState, mapGetters } from "vuex";
+export default {
+    computed: {
+        ...mapGetters("sites", ["isLoaded", "isLoading", "currentSite"]),
+    },
+    methods: {
+      ...mapActions("sites", ["getSite"])
+    },
+    created() {
+        this.getSite(this.$route.params.uri)
+        console.log(this.currentSite)
+    },
+}
+</script>
+
+<style lang="css">
+</style>
