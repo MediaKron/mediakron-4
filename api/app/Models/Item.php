f1f4d13e4ab85108b682cf694ab96d03b39bad1f
Brad Mering <brad.mering@gmail.com>
2019-01-02 13:59:56 -0700

    Improving image thumbnail in relationships

--- api/app/Models/Item.php
+++ api/app/Models/Item.php
@@ -29,6 +29,10 @@ class Item extends BaseModel
         'attachments'
     ];
 
+    protected $appends = [
+        'thumbnail'
+    ];
+
     /**
      * The "booting" method of the model.
      *
@@ -185,20 +189,14 @@ class Item extends BaseModel
      * @param [type] $value
      * @return void
      */
-    public function getImageAttribute($value)
+    public function getThumbnailAttribute($value)
     {
-        return unserialize($value);
-    }
-
-    /**
-     * Set the roles for the user as a serialized array
-     *
-     * @param [type] $roles
-     * @return void
-     */
-    public function setImageAttribute($metadata)
-    {
-        $this->attributes['image'] = serialize($metadata);
+        if($this->image){
+            if(isset($this->image->url)){
+                return $this->image->url;
+            }
+        }
+        return false;
     }
 
     /**
@@ -213,59 +211,12 @@ class Item extends BaseModel
 
         $data = [ 
             'id' => $this->id,
-            'title' => $this->title
+            'title' => $this->title,
+            'type' => $this->type,
+            'image' => $this->thumbnail
         ];
         return $data;
     }
 
-    /**
-     * Get the model's relationships in array form.
-     *
-     * @return array
-     */
-    public function relationsToArray()
-    {
-        $attributes = [];
-        dd($this->parents);
-        foreach ($this->getArrayableRelations() as $key => $value) {
-            // If the values implements the Arrayable interface we can just call this
-            // toArray method on the instances which will convert both models and
-            // collections to their proper array form and we'll set the values.
-            if ($value instanceof Arrayable) {
-                if ($value instanceof BaseCollection) {
-                    dd($value->pivot);
-                    //$item->toRelationshipArray();
-                } else {
-                    $relation = $value->toRelationshipArray();
-                }
-
-            }
-
-            // If the value is null, we'll still go ahead and set it in this list of
-            // attributes since null is used to represent empty relationships if
-            // if it a has one or belongs to type relationships on the models.
-            elseif (is_null($value)) {
-                $relation = $value;
-            }
-
-            // If the relationships snake-casing is enabled, we will snake case this
-            // key so that the relation attribute is snake cased in this returned
-            // array to the developers, making this consistent with attributes.
-            if (static::$snakeAttributes) {
-                $key = Str::snake($key);
-            }
-
-            // If the relation value has been set, we will set it on this attributes
-            // list for returning. If it was not arrayable or null, we'll not set
-            // the value on the array because it is some type of invalid value.
-            if (isset($relation) || is_null($value)) {
-                $attributes[$key] = $relation;
-            }
-
-            unset($relation);
-        }
-
-        return $attributes;
-    }
 
 }
