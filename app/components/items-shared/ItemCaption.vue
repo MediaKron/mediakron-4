906c838ebbd5965be2db4b59aedde6d4fbe2a473
Tim Lindgren <contact@timlindgren.com>
2019-02-21 14:12:16 -0500

    Tweaks

--- app/components/items-shared/ItemCaption.vue
+++ app/components/items-shared/ItemCaption.vue
@@ -1,5 +1,5 @@
 <template>
-     <div v-if="isEditing">
+     <div class="item-element-caption" v-if="isEditing">
          <b-form-group class="floating-label">
             <b-form-textarea id="caption" v-model="editItem.caption" :rows="3" placeholder="Add a caption"></b-form-textarea>
             <label for="caption">Caption</label>
