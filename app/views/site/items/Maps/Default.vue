622aa1193fc5c9dce5182f3de055db9e9f80ba6f
Brad Mering <brad.mering@gmail.com>
2019-02-27 09:57:58 -0700

    Adding maps stubs

---
+++ app/views/site/items/Maps/Default.vue
@@ -0,0 +1,45 @@
+<template>
+    <article class="container">
+        <ItemTitle></ItemTitle>
+        <ItemDescription></ItemDescription>
+        
+        <ItemCaption></ItemCaption>
+    </article>
+</template>
+
+<script>
+import ItemTitle from '@/components/items-shared/ItemTitle'
+import ItemDescription from '@/components/items-shared/ItemDescription'
+import ItemCaption from '@/components/items-shared/ItemCaption'
+import Image from '@/components/items-shared/Image'
+
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    props: [ 'item' ],
+    components: {
+        ItemTitle,
+        ItemDescription,
+        ItemCaption,
+        Image
+    },
+    created() {
+
+    },
+    computed: {
+        ...mapGetters('items', [
+            'editItem',
+            'isEditing'
+        ])
+    },
+    methods: {
+        ...mapActions('items', [
+            'setEditItem',
+        ])
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app/views/site/items/Maps/Full.vue
@@ -0,0 +1,43 @@
+<template>
+    <article class="container">
+        <ItemTitle></ItemTitle>
+        <ItemDescription></ItemDescription>
+        <item-image></item-image>
+        <ItemCaption></ItemCaption>
+    </article>
+</template>
+
+<script>
+import ItemTitle from '@/components/items-shared/ItemTitle'
+import ItemDescription from '@/components/items-shared/ItemDescription'
+import ItemCaption from '@/components/items-shared/ItemCaption'
+import ItemImage from '@/components/items-shared/Image'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    props: [ 'item' ],
+    components: {
+        ItemTitle,
+        ItemDescription,
+        ItemCaption,
+        ItemImage
+    },
+    created() {
+
+    },
+    computed: {
+        ...mapGetters('items', [
+            'editItem'
+        ])
+    },
+    methods: {
+        ...mapActions('items', [
+            'setEditItem'
+        ])
+    }
+}
+</script>
+
+<style>
+
+</style>
