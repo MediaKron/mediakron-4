380de990714246900e243cc0872be0d98d48fecb
james walker <walkerjj@bc.edu>
2019-03-11 17:34:52 -0400

    initial video format templates

--- app/components/items-shared/Video.vue
+++ app/components/items-shared/Video.vue
@@ -11,9 +11,14 @@
                 placeholder="Choose a file..."
                 drop-placeholder="Drop file here..."
                 :accept="first.allowedTypes()"/>
-        <b-progress height="2rem" v-if="isUploading" :value="counter" :max="max" show-progress animated />
+
+                <b-progress height="2rem" v-if="isUploading" :value="counter" :max="max" show-progress animated />
+
+        <vue-multiselect  v-model="typeFormat" :options="typeOptions" class="mr-2 border border-dark rounded" track-by="value" label="text" placeholder="Choose Video Format:">
+        </vue-multiselect>
     </b-form-group>
-    <div v-else>
+
+        <div v-else>
         <component :is="player"></component>
     </div>
 </template>
@@ -21,13 +26,23 @@
 <script>
     import { mapGetters } from 'vuex'
     import Youtube from '@/components/players/Youtube'
+    import Multiselect from 'vue-multiselect'
+    import VueMultiselect from "../../../node_modules/vue-multiselect/src/Multiselect";
     export default {
+        components: {VueMultiselect},
         name: 'Component',
         mounted() {
             console.log(this.player)
         },
         data() {
             return {
+                typeFormat: [],
+                typeOptions: [
+                    {value:'youtube', text: 'Youtube'},
+                    {value:'google', text: 'Google'},
+                    {value:'panopto', text: 'Panopto'},
+                    {value:'vimeo', text: 'Vimeo'},
+                ],
             }
         },
         computed: {
@@ -35,6 +50,10 @@
                 switch(this.first.video.type){
                     case 'youtube':
                         return Youtube
+                    case 'google':
+                        return Google
+                    case 'panopto':
+                        return Panopto
                 }
             },
             ...mapGetters('items', [
