ba6e0d41cd5c5dd0830f88c780a0f2ebfceb6023
Tim Lindgren <contact@timlindgren.com>
2019-03-12 13:48:13 -0400

    Prototyping half and full page banner components

---
+++ app/components/banners/BannerFull.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="banner-full background-image background-image-darkened bg-cover flex flex-col items-center justify-center text-white">
+    <slot></slot>
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .banner-full {
+        height: 90vh;
+    }
+</style>
+
---
+++ app/components/banners/BannerHalf.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="banner-half background-image background-image-darkened bg-cover flex flex-col items-center justify-center text-white">
+    <slot></slot>
+</div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .banner-half {
+        height: 42vh;
+    }
+</style>
+
