2d241e8dc2c78d28eff045ee700dde6637dbbc58
Jamie Walker <walkerjj@cteone0250s6md.bc.edu>
2019-04-10 16:23:35 -0400

    updates to a/v player includes in items

--- app/views/site/content/AllContent.vue
+++ app/views/site/content/AllContent.vue
@@ -33,7 +33,7 @@
         <b-table v-if="listIsLoaded" :items="items" :busy="isBusy" :fields="fields" :filter="filter"
             @filtered="onFiltered" sortBy="updated_at" sort-desc="true" stacked="md">
             <template slot="title" slot-scope="items">
-                <b-img :src="items.item.imageUrl('small')" blank-color="#777" alt="Placeholder Image" />
+                <b-img class="thumb" :src="items.item.imageUrl('small')" blank-color="#777" alt="Placeholder Image" />
                 <router-link :to="items.item.uri" class="font-bold text-1xl">{{ items.item.title }}
                 </router-link>
             </template>
@@ -128,5 +128,5 @@
 </script>
 
 <style>
-
+.thumb {width:75px; height:75px;}
 </style>
