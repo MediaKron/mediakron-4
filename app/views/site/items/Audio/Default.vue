c41d12b4928c71fed4b6432a6d7dd9bcb7072bf7
Tim Lindgren <contact@timlindgren.com>
2019-04-22 18:22:09 -0400

    Standardizing naming conventions for item components

--- app/views/site/items/Audio/Default.vue
+++ app/views/site/items/Audio/Default.vue
@@ -1,32 +1,39 @@
 <template>
   <article class="container max-w-lg mx-auto">
-        <ItemTitle></ItemTitle>
-        <ItemCaption></ItemCaption>
-        <Audio></Audio>
-        <ItemDescription></ItemDescription>
-        <ItemMetadata></ItemMetadata>
-        <ItemTags></ItemTags>
+        <m-title></m-title>
+        <m-caption></m-caption>
+        <m-audio></m-audio>
+        <m-description></m-description>
+        <m-metadata></m-metadata>
+        <m-tags></m-tags>
    </article>
 </template>
 
 <script>
-import ItemTitle from '@/components/items-shared/ItemTitle'
-import ItemDescription from '@/components/items-shared/ItemDescription'
-import ItemCaption from '@/components/items-shared/ItemCaption'
-import Audio from '@/components/items-shared/Audio'
-import ItemMetadata from '@/components/items-shared/ItemMetadata'
-import ItemTags from '@/components/items-shared/ItemTags'
+import MTitle from '@/components/items-shared/Title'
+import MDescription from '@/components/items-shared/Description'
+import MCaption from '@/components/items-shared/Caption'
+import MVideo from '@/components/items-shared/Video'
+import MMetadata from '@/components/items-shared/Metadata'
+import MTags from '@/components/items-shared/Tags'
+import MCustomDate from '@/components/items-shared/CustomDate'
+import MImage from '@/components/items-shared/Image'
+
+import MAudio from '@/components/items-shared/Audio'
+
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
     props: [ 'item', 'isEditing' ],
     components: {
-        ItemTitle,
-        ItemDescription,
-        ItemCaption,
-        Audio,
-        ItemMetadata,
-        ItemTags
+        MTitle,
+        MDescription,
+        MCaption,
+        MImage,
+        MMetadata,
+        MTags,
+        MCustomDate,
+        MAudio
     },
     created() {
     },
