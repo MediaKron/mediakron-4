e58e97d665b537f5632d84646e2f1ada25490acc
Tim Lindgren <contact@timlindgren.com>
2019-05-02 14:50:05 -0400

    Vertical item buttons

--- app/views/site/items/Images/index.vue
+++ app/views/site/items/Images/index.vue
@@ -3,7 +3,7 @@
         <component :is="component" :item="item"/>
         <div class="right-0 top-0 fixed mr-4 mt-20 flex flex-column ">
             <b-button v-if="!isEditing" class="uppercase mb-2 flex flex-column items-center bg-light border-none item-button " variant="outline-dark" size="sm" @click="editClicked"><font-awesome-icon icon="edit" class="text-lg mt-1"/> <span class="text-xs mt-1">Edit</span></b-button>  
-            <b-button v-if="isEditing" class="uppercase" variant="outline-dark" size="lg" @click="saveClicked"> <font-awesome-icon icon="check" /> Save </b-button>       
+            <b-button v-if="isEditing" class="uppercase mb-2 flex flex-column items-center border-none item-button " variant="primary" size="lg" @click="saveClicked"> <font-awesome-icon icon="check" class="text-lg mt-1"/> <span class="text-xs mt-1">Save </span></b-button>       
             <m-options></m-options>
 
         </div>
