6889f41d44a0775896bd7531e2397cfe67a01a31
Tim Lindgren <contact@timlindgren.com>
2019-05-21 16:51:27 -0400

    Creating components for Save, Edit, Cancel buttons

--- app/views/site/items/Images/index.vue
+++ app/views/site/items/Images/index.vue
@@ -2,16 +2,13 @@
     <div>
         <component :is="component" :item="item" />
         <div class="right-0 top-0 fixed mr-6 mt-20 flex flex-column ">
-            <b-button v-if="!isEditing" class="uppercase mb-2 flex flex-column items-center bg-light border-none item-button" variant="outline-dark" size="sm" @click="editClicked">
+             <b-button v-if="!isEditing" class="uppercase mb-2 flex flex-column items-center bg-light border-none item-button" variant="outline-dark" size="sm" @click="editClicked">
                 <font-awesome-icon icon="edit" class="text-lg mt-1" /> <span class="text-xs mt-1">Edit</span>
             </b-button>
+            <!-- <m-edit-button v-if="!isEditing" ></m-edit-button> -->
             <m-options v-if="!isEditing"></m-options>
-            <b-button v-if="isEditing" class="uppercase mb-2 flex flex-column items-center border-none item-button" variant="primary" size="sm" @click="saveClicked">
-                <font-awesome-icon icon="check" class="text-lg mt-1" /> <span class="text-xs mt-1">Save </span>
-            </b-button>
-            <b-button v-if="isEditing" class="uppercase mb-2 flex flex-column items-center bg-light border-none item-button" size="sm" variant="outline-dark">
-                <font-awesome-icon icon="times" class="text-lg mt-1"/> <span class="text-xxs mt-1">Cancel</span>
-            </b-button>
+            <m-save-button v-if="isEditing" ></m-save-button>
+            <m-cancel-button v-if="isEditing" ></m-cancel-button>
         </div>
 
     </div>
@@ -19,15 +16,21 @@
 
 <script>
     import Default from './Default'
-    import editable from '~/components/mixins/editable';
+    import editable from '~/components/mixins/editable'
     import MOptions from '@/components/items-shared/Options'
+    import MSaveButton from '@/components/forms/SaveButton'
+    import MEditButton from '@/components/forms/EditButton'
+    import MCancelButton from '@/components/forms/CancelButton'
     import {
         mapGetters,
         mapActions
     } from 'vuex'
     export default {
         components: {
-            MOptions
+            MOptions,
+            MSaveButton,
+            MEditButton,
+            MCancelButton
         },
         mixins: [editable],
         props: ['item'],
